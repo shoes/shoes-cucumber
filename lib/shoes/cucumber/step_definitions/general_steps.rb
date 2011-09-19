0100382dd677c8a9f05a52a06f123429df1c895f
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-20 00:13:27 +0200

    We now may check for the presence of x things of a type

--- lib/shoes/cucumber/step_definitions/general_steps.rb
+++ lib/shoes/cucumber/step_definitions/general_steps.rb
@@ -1,5 +1,19 @@
 NAME_METHOD_MAPPING = { "button" => "button",
-                           "paragraph" => "para" }
+                        "paragraph" => "para",
+                        "link" => "link" }
+
+def singularize(string)
+  # for now it's easy enough, get rid of the s
+  if string[-1] == "s"
+    string.chop
+  else
+    string
+  end
+end
+
+def class_for_element(element_name)
+  Shoes.const_get(singularize(element_name).capitalize)
+end
 
 Given /^a Shoes application$/ do
   @app = Shoes.app
@@ -18,14 +32,19 @@ When /^I append an? ([^"]+) to the main window$/ do |element_name|
 end
 
 Then /^I should see an? ([^"]+)$/ do |element_name|
-  class_name = Shoes.const_get(element_name.capitalize)
+  class_name = class_for_element(element_name)
   @app.elements.find{|e| e.kind_of? class_name}.should_not be_nil
 end
 
 Then /^I should see an? ([^"]+) with text "([^"]+)"$/ do |element_name, text|
-  class_name = Shoes.const_get(element_name.capitalize)
+  class_name = class_for_element(element_name)
   @app.elements.find do |e|
     e.kind_of?(class_name) && e.text == text
   end.should_not be_nil
+end
+
+Then /^I should see (\d+) ([^"]+)$/ do |number, element_name|
+  class_name = class_for_element(element_name)
+  @app.elements.find_all{|e| e.kind_of? class_name}.size.should be number.to_i
 end
 
