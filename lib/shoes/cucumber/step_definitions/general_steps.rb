2c437b6d0a33d2bd57c5bb7a96d70abf0b035c70
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-23 22:38:21 +0200

    Steps for detecting that a popup has been opened

--- lib/shoes/cucumber/step_definitions/general_steps.rb
+++ lib/shoes/cucumber/step_definitions/general_steps.rb
@@ -4,7 +4,7 @@ Given /^a Shoes application$/ do
   @app = Shoes.app
 end
 
-Given /^a Shoes application in "([^"]+)"$/ do |file_path|
+Given /^the Shoes application in "([^"]+)"$/ do |file_path|
   # load as I want a fresh file every time
   load './' + file_path
   @app = Shoes.application
@@ -23,20 +23,40 @@ When /^I append an? ([^"]+) to the main window$/ do |element_name|
   end
 end
 
+When /^I click the ([^"]+) labeled "([^"]+)"$/ do |element_name, text|
+  class_name = class_for(element_name)
+  button = @app.elements.find do |element|
+    element.kind_of?(class_name) && element.text == text
+  end
+  button.click
+end
+
 Then /^I should see an? ([^"]+)$/ do |element_name|
   class_name = class_for(element_name)
-  @app.elements.find{|e| e.kind_of? class_name}.should_not be_nil
+  @app.elements.find{ |element| element.kind_of?(class_name) }.should_not be_nil
 end
 
 Then /^I should see an? ([^"]+) with text "([^"]+)"$/ do |element_name, text|
   class_name = class_for(element_name)
-  @app.elements.find do |e|
-    e.kind_of?(class_name) && e.text == text
+  @app.elements.find do |element|
+    element.kind_of?(class_name) && element.text == text
   end.should_not be_nil
 end
 
 Then /^I should see (\d+) ([^"]+)$/ do |number, element_name|
   class_name = class_for(element_name)
-  @app.elements.find_all{|e| e.kind_of? class_name}.size.should be number.to_i
+  @app.elements.find_all do |element|
+    element.kind_of? class_name
+  end.size.should be number.to_i
+end
+
+Then /^a popup should appear$/ do
+  @app.events.find { |element| element.kind_of?(Shoes::Alert) }.should_not be_nil
+end
+
+Then /^a popup with text "([^"]+)" should appear$/ do |text|
+  @app.events.find do |element|
+    element.kind_of?(Shoes::Alert) && element.text == text
+  end.should_not be_nil
 end
 
